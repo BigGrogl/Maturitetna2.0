05c64a86ba5d92eb7d11bf56b6bb36e86636242e
Grogl <ziga2003.groselj@gmail.com>
2022-04-13 11:16:23 +0200

    started GUI

---
+++ GUI.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.*;
+
+public class GUI {
+
+    public static File selectedFile;
+    public static void main(String[] args){
+        JFrame frame = new JFrame("TEST GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+
+        JButton buttonEncode = new JButton("Encode");
+        JButton buttonDecode = new JButton("Decode");
+
+       /* frame.getContentPane().add(buttonEncode);
+        frame.getContentPane().add(buttonDecode);
+        frame.setVisible(true);*/
+
+        JPanel panel = new JPanel();
+        panel.add(buttonEncode);
+        panel.add(buttonDecode);
+
+        JPanel panelDown = new JPanel();
+        
+
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(panel);
+        if(result == JFileChooser.APPROVE_OPTION){
+            selectedFile = fileChooser.getSelectedFile();
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+        }
+
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+
+    }
+
+    public static String path(){
+        return selectedFile.getAbsolutePath();
+    }
+    public static String pathWrite(){
+        return path.substring(0, path.lastIndexOf(File.separator));
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -1,28 +1,29 @@
 import java.util.*;
 
 public class Main {
-    public static String PathCripted = "D:\\GIT\\Maturitetna2.0\\Cripted.txt";
-    public static String PathPlain = "D:\\GIT\\Maturitetna2.0\\Plain.txt";
-    public static String PathDecripted = "D:\\GIT\\Maturitetna2.0\\Decripted.txt";
+    //public static String PATH = GUI.Path();
+    public static String PathCripted = GUI.pathWrite(GUI.path());
+    public static String PathPlain = GUI.path();
+    public static String PathDecripted = "C:\\Grošelj\\Maturitetna2.0\\Decripted.txt";
     public static Scanner bralnik = new Scanner(System.in);
     public static String Key = "";
+
     public static void main(String[] args){
+        GUI.main(args);
+        /*
         System.out.printf("%nVnesi kljucno besedo: ");
         Key = bralnik.nextLine();
         System.out.printf("Izberite funkcijo (Encript/Decript)! ");
         String funkcija = bralnik.next();
         if(funkcija.equals("Encript")){
-        Reader.Write(Logic.encode(Reader.Read(PathPlain), Key), PathCripted);
+        Reader.Write(Logic.encode(Reader.Read(PATH), Key), PathCripted);
         }else if(funkcija.equals("Decript")){
         Reader.Write(Logic.decode(Reader.Read(PathCripted), Key), PathDecripted);
             if(Reader.Read(PathPlain).equals(Reader.Read(PathDecripted))){
                 System.out.println("DELA!");
             }else{
-                System.out.println("RETARD SI!");
+                System.out.println("NE DELA!");
             }
-        }
-        
-
-        
+        }*/
     }
 }
